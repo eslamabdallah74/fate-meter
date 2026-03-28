69b115289036b91b8514dd8227fcfd03ef11df11
eslamabdallah74 <eslamabdallah301@gmail.com>
2026-03-28 11:51:54 +0200

    add deply

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// Change '/fatemeter/' to '/' if using a user site (username.github.io)
 export default defineConfig({
   plugins: [vue()],
+  base: '/fate-meter/'
 })
